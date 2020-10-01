2c7276542a0673ba70b2c5e9b7631d5b11743429
Simran Sahu <simransahupnt@gmail.com>
2020-10-01 13:17:47 +0530

    Add files via upload

---
+++ array2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5], i;
+	printf("Size of integer in the compiler is %lu\n", sizeof(int));
+
+	for (i=0;i<5; i++)
+		printf("Address arr[%d] is %p\n", i, &arr[i]);
+	return 0;
+}
